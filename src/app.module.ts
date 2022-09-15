b65634edb9e8caae1c560376aa36188d5dd4e978
이종은 <lje0818@midasin.com>
2022-09-15 15:52:24 +0900

    [ADD] another module for AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,24 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ArticleController } from './article/article.controller';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {ArticleController} from './article/article.controller';
 import {ArticleService} from "./article/article.service";
-import { AuthController } from './auth/auth.controller';
+import {AuthController} from './auth/auth.controller';
 import {MongooseModule} from "@nestjs/mongoose";
 import {AuthService} from "./auth/auth.service";
+import {ArticleModule} from "./article/article.module";
+import {AuthModule} from "./auth/auth.module";
 
 @Module({
-  controllers: [AppController, AuthController],
-  providers: [AppService,  AuthService],
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017', {
-    dbName: 'article'
-  })]
+    imports: [
+        MongooseModule.forRoot('mongodb://localhost:27017', {
+            dbName: 'conduit',
+        }),
+        ArticleModule,
+        AuthModule
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
